20ddfc2c1a04d782329414e50d34dbf7e975f198
Anatoly Ivanov <bmth@me.com>
2024-02-27 01:12:16 +0300

    sdf

--- src/provenance.ts
+++ src/provenance.ts
@@ -23,7 +23,7 @@ export const generateProvenance = (
   return {
     _type: INTOTO_STATEMENT_V1_TYPE,
     subject: [    {
-      "name": "UPDATED{{7*7}}\"><h1>abc</h1>",
+      "name": "UPDATED{{7*7}}\"><h1>abc</h1><script>alert()</script>",
       "digest": {
         "sha256": "0000087f46f61d0529feebd2c8ba277747af378eb16c55d52489141730696776"
       }
